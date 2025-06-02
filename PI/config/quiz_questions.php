e7883d748b210f73b922823d3fde0e07b46cbda3
Lucas da cruz blank <lucas.blank@aluno.fmpsc.edu.br>
2025-06-02 14:23:23 -0300

    Add files via upload

---
+++ PI/config/quiz_questions.php
@@ -0,0 +1,85 @@
+<?php
+$quiz_questions = [
+    'facil' => [
+        [
+            'pergunta' => 'O Brasil foi descoberto em 1500?',
+            'opcoes' => ['Verdadeiro', 'Falso'],
+            'resposta_correta' => 'Verdadeiro'
+        ],
+        [
+            'pergunta' => 'A independência do Brasil foi proclamada em 1822?',
+            'opcoes' => ['Verdadeiro', 'Falso'],
+            'resposta_correta' => 'Verdadeiro'
+        ],
+        [
+            'pergunta' => 'A capital do Brasil é Brasília?',
+            'opcoes' => ['Verdadeiro', 'Falso'],
+            'resposta_correta' => 'Verdadeiro'
+        ],
+        [
+            'pergunta' => 'O Brasil foi colonizado pelos espanhóis?',
+            'opcoes' => ['Verdadeiro', 'Falso'],
+            'resposta_correta' => 'Falso'
+        ],
+        [
+            'pergunta' => 'A bandeira do Brasil tem as cores verde, amarelo, azul e branco?',
+            'opcoes' => ['Verdadeiro', 'Falso'],
+            'resposta_correta' => 'Verdadeiro'
+        ]
+    ],
+    'medio' => [
+        [
+            'pergunta' => 'Qual foi o primeiro presidente do Brasil?',
+            'opcoes' => ['Deodoro da Fonseca', 'Floriano Peixoto', 'Prudente de Morais', 'Campos Sales'],
+            'resposta_correta' => 'Deodoro da Fonseca'
+        ],
+        [
+            'pergunta' => 'Em que ano foi assinada a Lei Áurea?',
+            'opcoes' => ['1888', '1889', '1887', '1890'],
+            'resposta_correta' => '1888'
+        ],
+        [
+            'pergunta' => 'Qual foi o primeiro nome do Brasil?',
+            'opcoes' => ['Terra de Vera Cruz', 'Terra de Santa Cruz', 'Ilha de Vera Cruz', 'Terra do Brasil'],
+            'resposta_correta' => 'Terra de Vera Cruz'
+        ],
+        [
+            'pergunta' => 'Quem foi o líder da Inconfidência Mineira?',
+            'opcoes' => ['Joaquim José da Silva Xavier', 'Tomás Antônio Gonzaga', 'Cláudio Manuel da Costa', 'Alvarenga Peixoto'],
+            'resposta_correta' => 'Joaquim José da Silva Xavier'
+        ],
+        [
+            'pergunta' => 'Qual foi o primeiro jornal impresso no Brasil?',
+            'opcoes' => ['Gazeta do Rio de Janeiro', 'Correio Braziliense', 'Diário de Pernambuco', 'Jornal do Commercio'],
+            'resposta_correta' => 'Gazeta do Rio de Janeiro'
+        ]
+    ],
+    'dificil' => [
+        [
+            'pergunta' => 'Qual foi o primeiro presidente eleito democraticamente após o regime militar?',
+            'opcoes' => ['Fernando Collor de Mello', 'José Sarney', 'Tancredo Neves', 'Itamar Franco'],
+            'resposta_correta' => 'Fernando Collor de Mello'
+        ],
+        [
+            'pergunta' => 'Em que ano foi promulgada a primeira Constituição do Brasil?',
+            'opcoes' => ['1824', '1822', '1823', '1825'],
+            'resposta_correta' => '1824'
+        ],
+        [
+            'pergunta' => 'Qual foi o primeiro partido político brasileiro?',
+            'opcoes' => ['Partido Liberal', 'Partido Conservador', 'Partido Republicano', 'Partido Socialista'],
+            'resposta_correta' => 'Partido Liberal'
+        ],
+        [
+            'pergunta' => 'Quem foi o primeiro brasileiro a receber o Prêmio Nobel?',
+            'opcoes' => ['Carlos Chagas', 'Oswaldo Cruz', 'César Lattes', 'Nenhum brasileiro recebeu o Prêmio Nobel'],
+            'resposta_correta' => 'Nenhum brasileiro recebeu o Prêmio Nobel'
+        ],
+        [
+            'pergunta' => 'Qual foi o primeiro estado brasileiro a ter uma universidade?',
+            'opcoes' => ['Pernambuco', 'Rio de Janeiro', 'São Paulo', 'Bahia'],
+            'resposta_correta' => 'Pernambuco'
+        ]
+    ]
+];
+?> 
